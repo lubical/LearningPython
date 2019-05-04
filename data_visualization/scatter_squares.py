a24c7bc71c48e82a60f141b75229cd052d7340cd
lubical <691483523@qq.com>
2019-05-04 12:43:54 +0800

    matplotlib.plt and pygal

--- data_visualization/scatter_squares.py
+++ data_visualization/scatter_squares.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=40)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
@@ -17,3 +17,4 @@ plt.tick_params(axis='both', which='major', labelsize=14)
 plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
+#plt.savefig('squares_plot.png', bbox_inches='tight')
